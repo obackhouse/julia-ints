0f0bccba8e1a541e0a68701c8b23e5a5e878d240
Ollie <olbackhouse@gmail.com>
2021-03-20 14:20:49 +0000

    Refactors expansion coefficient functions

--- src/expansion.jl
+++ src/expansion.jl
@@ -33,10 +33,10 @@ function mcmurchie_davidson_i(i, j, t, KAB, PA, PB, p, q)
     #  i,j     1    i-1,j        i-1,j          i-1,j
     # E    = ----- E      + X   E      + (t+1) E
     #  t      2*p   t-1      PA  t              t+1
-    
-    Eab = expansion(i-1, j, t-1, KAB, PA, PB, p, q) .* q
-    Eab = Eab .+ expansion(i-1, j, t,   KAB, PA, PB, p, q) .* PA
-    Eab = Eab .+ expansion(i-1, j, t+1, KAB, PA, PB, p, q) * (t+1)
+
+    Eab = expansion(i-1, j, t-1, KAB, PA, PB, p, q) .* q .+
+          expansion(i-1, j, t,   KAB, PA, PB, p, q) .* PA .+
+          expansion(i-1, j, t+1, KAB, PA, PB, p, q) * (t+1)
 
     Eab
 end
@@ -47,9 +47,9 @@ function mcmurchie_davidson_j(i, j, t, KAB, PA, PB, p, q)
     # E    = ----- E      + X   E      + (t+1) E
     #  t      2*p   t-1      PA  t              t+1
 
-    Eab = expansion(i, j-1, t-1, KAB, PA, PB, p, q) .* q
-    Eab = Eab .+ expansion(i, j-1, t,   KAB, PA, PB, p, q) .* PB
-    Eab = Eab .+ expansion(i, j-1, t+1, KAB, PA, PB, p, q) * (t+1)
+    Eab = expansion(i, j-1, t-1, KAB, PA, PB, p, q) .* q .+
+          expansion(i, j-1, t,   KAB, PA, PB, p, q) .* PB .+
+          expansion(i, j-1, t+1, KAB, PA, PB, p, q) * (t+1)
 
     Eab
 end
@@ -60,15 +60,8 @@ function two_term(i, j, t, KAB, PA, PB, p, q)
     # E    = ----- | i E      + j E      |   for t > 0
     #  t      2pt  \    t-1        t-1   /
     
-    Eab = zero(KAB)
-
-    if i > 0
-        Eab = Eab .+ expansion(i-1, j, t-1, KAB, PA, PB, p, q) * i
-    end
-    if j > 0
-        Eab = Eab .+ expansion(i, j-1, t-1, KAB, PA, PB, p, q) * j
-    end
-    Eab = Eab .* (q / t)
+    Eab = (expansion(i-1, j,   t-1, KAB, PA, PB, p, q) * i .+
+           expansion(i,   j-1, t-1, KAB, PA, PB, p, q) * j) .* (q / t)
 
     Eab
 end
@@ -79,22 +72,18 @@ function order_dump_i(i, j, t, KAB, PA, PB, p, q)
     # E    = | ----- |   |   | E
     #  t     \  2*p  /   \ t /  0
 
-    Eab = expansion(i-t, 0, 0, KAB, PA, PB, p, q)
-    Eab = Eab .* binomial(i, t)
-    Eab = Eab .* q.^t
+    Eab = expansion(i-t, 0, 0, KAB, PA, PB, p, q) .* q.^t * binomial(i, t)
 
     Eab
 end
 
 
 function order_dump_j(i, j, t, KAB, PA, PB, p, q)
-    #  0,j   /   1   \ t / i \  0,j-t
+    #  0,j   /   1   \ t / j \  0,j-t
     # E    = | ----- |   |   | E
-    #  t     \  2*p  /   \ j /  0
+    #  t     \  2*p  /   \ t /  0
 
-    Eab = expansion(0, j-t, 0, KAB, PA, PB, p, q)
-    Eab = Eab .* binomial(j, t)
-    Eab = Eab .* q.^t
+    Eab = expansion(0, j-t, 0, KAB, PA, PB, p, q) .* q.^t * binomial(j, t)
 
     Eab
 end
@@ -104,18 +93,12 @@ function obara_saika_i(i, j, t, KAB, PA, PB, p, q)
     #  i,j        i-1,j     1   /        i-2,j      i-1,j-1    i-1,j \
     # E    = X   E      + ----- | (i-1) E      + j E        + E      |
     #  t      PA  t        2*p  \        t          t          t-1   /
-    
-    Eab = zero(KAB)
 
-    if i > 1
-        Eab = Eab .+ expansion(i-2, j, t, KAB, PA, PB, p, q) * (i-1)
-    end
-    if j > 0
-        Eab = Eab .+ expansion(i-1, j-1, t, KAB, PA, PB, p, q) * j
-    end
-    Eab = Eab .+ expansion(i-1, j, t-1, KAB, PA, PB, p, q)
-    Eab = Eab .* q
-    Eab = Eab .+ expansion(i-1, j, t, KAB, PA, PB, p, q) .* PA
+    Eab = expansion(i-1, j,   t,   KAB, PA, PB, p, q) .* PA .+ q .* (
+          expansion(i-2, j,   t,   KAB, PA, PB, p, q) * (i-1) .+
+          expansion(i-1, j-1, t,   KAB, PA, PB, p, q) * j .+
+          expansion(i-1, j,   t-1, KAB, PA, PB, p, q)
+    )
 
     Eab
 end
@@ -126,17 +109,11 @@ function obara_saika_j(i, j, t, KAB, PA, PB, p, q)
     # E    = X   E      + ----- | i E        + (j-1) E      + E      |
     #  t      PB  t        2*p  \    t                t        t-1   /
     
-    Eab = zero(KAB)
-
-    if i > 0
-        Eab = Eab .+ expansion(i-1, j-1, t, KAB, PA, PB, p, q) * i
-    end
-    if j > 1 
-        Eab = Eab .+ expansion(i, j-2, t, KAB, PA, PB, p, q) * (j-1)
-    end
-    Eab = Eab .+ expansion(i, j-1, t-1, KAB, PA, PB, p, q)
-    Eab = Eab .* q
-    Eab = Eab .+ expansion(i, j-1, t, KAB, PA, PB, p, q) .* PB
+    Eab = expansion(i,   j-1, t,   KAB, PA, PB, p, q) .* PB .+ q .* (
+          expansion(i-1, j-1, t,   KAB, PA, PB, p, q) * i .+
+          expansion(i,   j-2, t,   KAB, PA, PB, p, q) * (j-1) .+
+          expansion(i,   j-1, t-1, KAB, PA, PB, p, q)
+    )
 
     Eab
 end
@@ -158,6 +135,23 @@ function expansion(i, j, t, KAB, PA, PB, p, q)
         Eab = obara_saika_i(i, j, t, KAB, PA, PB, p, q)
     else
         Eab = obara_saika_j(i, j, t, KAB, PA, PB, p, q)
+    end
+
+    Eab
+end
+
+
+function expansion_mmd(i, j, t, KAB, PA, PB, p, q)
+    # Standard McMurchie-Davidson path
+
+    if (t < 0) || (t > (i+j)) || (i < 0) || (j < 0)
+        Eab = oob(i, j, t, KAB, PA, PB, p, q)
+    elseif i == j == t == 0
+        Eab = base(i, j, t, KAB, PA, PB, p, q)
+    elseif j == 0
+        Eab = mcmurchie_davidson_i(i, j, t, KAB, PA, PB, p, q)
+    else
+        Eab = mcmurchie_davidson_j(i, j, t, KAB, PA, PB, p, q)
     end
 
     Eab
