3a4852bc52783e56075ab360a0d5f5208f00cdc8
Ollie <olbackhouse@gmail.com>
2021-03-20 14:21:50 +0000

    Update test file as an example for now

--- test.jl
+++ test.jl
@@ -1,17 +1,32 @@
 include("src/driver.jl")
 
-basis = parse_basis(
-    "sto-3g", 
-    ["O", "H", "H"],
-    [[0.,0.,0.] [0.,0.,2.] [0.,2.,0.]]
-)
+#atoms = [("O", [0., 0., 0.]),
+#         ("H", [0., 2., 0.]),
+#         ("H", [0., 0., 2.])]
+atoms = [("O", [ 0.,            -0.143225816552, 0.]),
+         ("H", [ 1.638036840407, 1.136548822547, 0.]),
+         ("H", [-1.638036840407, 1.136548822547, 0.])]
 
-s = driver("int1e_ovlp", basis)
+basis = parse_basis(atoms, "sto-3g")
+#summarise(basis)
 
-show(stdout, "text/plain", s)
-println("")
 
-t = driver("int1e_kin", basis)
+#s = driver("int1e_ovlp", basis)
+#
+#show(stdout, "text/plain", s)
+#println("")
+#
+#t = driver("int1e_kin", basis)
+#
+#show(stdout, "text/plain", t)
+#println("")
+#
+#v = driver("int1e_nuc", basis)
+#
+#show(stdout, "text/plain", v)
+#println("")
 
-show(stdout, "text/plain", t)
+eri = driver("int2e", basis)
+
+show(stdout, "text/plain", sum(eri, dims=(3,4)))
 println("")
